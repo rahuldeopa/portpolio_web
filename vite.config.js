35c81c47e641aa8153e1d9dc3ad5a1cedb929600
rahuldeopa <rahuldeopa1818@gmail.com>
2025-01-25 23:30:23 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: 
-  [react(),
+  plugins: [
+    react(),
     tailwindcss(),
   ],
-})
+  server: {
+    allowedHosts: ['7r5g2c-5173.csb.app'], // Add your host here
+  },
+});
