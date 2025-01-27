c9a29e348b4f14b2f97ec8e9e83e9f37a16638a5
rahuldeopa <rahuldeopa1818@gmail.com>
2025-01-27 16:00:59 +0530

    added

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
   server: {
-    allowedHosts: ['7r5g2c-5173.csb.app'], // Add your host here
+    allowedHosts: ['https://rahuldeopa.github.io/portpolio_web/'], 
   },
 });
